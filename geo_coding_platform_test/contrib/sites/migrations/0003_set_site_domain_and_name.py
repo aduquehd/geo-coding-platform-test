61823e54bc86ef5bdb6a1d33ed52facd987322db
saduqz <saduqz@gmail.com>
2020-07-23 20:33:08 -0500

    Updated domain info

--- geo_coding_platform_test/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ geo_coding_platform_test/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "saduqz.com",
+            "domain": "geocoding.saduqz.com",
             "name": "Geo Coding Platform Test",
         },
     )
